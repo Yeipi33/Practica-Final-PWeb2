d8641a12f0144deb5d7d9609a34f029925075ed7
Yeipi33 <148468000+Yeipi33@users.noreply.github.com>
2026-04-03 11:57:14 +0200

    feat: /services/notification.service.js + /routes/user.routes.js
    añadidos imports y nuevas rutas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,19 @@
 //src/routes/user.routes.js
 import { Router } from "express";
-import { register } from "../controllers/user.controller.js";
+import { register, validateEmail } from "../controllers/user.controller.js";
 import { validate } from "../middleware/validate.js";
-import { registerSchema } from "../validators/auth.validators.js";
+import { registerSchema, validationCodeSchema } from "../validators/auth.validators.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", validate(registerSchema), register);
+
+router.put(
+  '/validation',
+  authMiddleware,
+  validate(validationCodeSchema),
+  validateEmail
+);
 
 export default router;
