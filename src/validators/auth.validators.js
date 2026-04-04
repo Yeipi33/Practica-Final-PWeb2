af2a9dc358f3e246754a8139970c2a882f530c57
Yeipi33 <148468000+Yeipi33@users.noreply.github.com>
2026-04-04 12:17:31 +0200

    feat: onboarding de datos del usuario y compañía

--- src/validators/auth.validators.js
+++ src/validators/auth.validators.js
@@ -1,4 +1,4 @@
-// src/validators/user.validator.js
+// src/validators/auth.validators.js
 import { z } from 'zod';
 
 export const registerSchema = z.object({
@@ -31,5 +31,44 @@ export const loginSchema = z.object({
     password: z
       .string({ required_error: 'La contraseña es requerida' })
       .min(8, 'Mínimo 8 caracteres'),
+  }),
+});
+
+const adressSchema = z.object({
+  street: z.string().trim().optional(),
+  number: z.string().trim().optional(),
+  postal: z.string().trim().optional(),
+  city: z.string().trim().optional(),
+  province: z.string().trim().optional(),
+});
+
+export const personalDataSchema = z.object({
+  body: z.object({
+    name: z
+      .string({ required_error: 'El nombre es requerido' })
+      .min(2, 'Minimo 2 caracteres')
+      .trim(),
+    lastName: z
+      .string({ required_error: 'Los apellidos son requeridos' })
+      .min(2, 'Minimo 2 caracteres')
+      .trim(),
+    nif: z
+      .string({ required_error: 'El NIF es requerido' })
+      .min(8, 'NIF no valido')
+      .trim()
+      .transform((val) => val.toUpperCase()),
+  }),
+});
+
+export const companyDataSchema = z.object({
+  body: z.object({
+    name: z.string().min(2, 'Minimo 2 caracteres').trim().optional(),
+    cif: z
+      .string({ required_error: 'El CIF es requerido' })
+      .min(8, 'CIF no valido')
+      .trim()
+      .transform((val) => val.toUpperCase()),
+    adress: adressSchema.optional(),
+    isFreelancer: z.boolean().optional(),
   }),
 });
