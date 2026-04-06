762bf33ef5af562ef40f98222566e914e50d2877
Yeipi33 <148468000+Yeipi33@users.noreply.github.com>
2026-04-06 17:39:27 +0200

    fix: conexion con mongo

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,10 @@
 
 import mongoose from 'mongoose';
 
+import dns from 'dns';
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const dbConnect = async () => {
     const DB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/PracticaIntermediaPWeb2';
 
