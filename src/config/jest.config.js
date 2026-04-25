27ac0ea15d0114c8aa01d529b26c2fa70b687b59
Yeipi33 <148468000+Yeipi33@users.noreply.github.com>
2026-04-25 11:31:32 +0200

    feat: añadido jest.config

---
+++ src/config/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.js'],
+  transform: {},
+  testMatch: ['**/tests/**/*.test.js'],
+  forceExit: true,
+  detectOpenHandles: true
+}
